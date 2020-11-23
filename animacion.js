b6780819d837de1e051d60defdf509d76b303d5e
Natalia2004-10 <natalia.nino1004@gmail.com>
2020-11-22 22:13:53 -0500

    Intento tarea1

---
+++ animacion.js
@@ -0,0 +1,67 @@
+let movimiento = document.querySelector("#movimiento");
+let obj2 = document.querySelector("#obj2");
+obj2.style.transform = `translate(0px, 449px)`;
+function cambiarColor(){
+//Es una función par crear colores aletaorios
+    movimiento.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    obj2.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);//javascript convierte de base decimal a base 16 con ese comando
+ }
+
+ let x = 0;
+ let dX = 25;
+ let y = 0;
+ let dY = 10;
+ let angle = 0;
+ let dAngle = 5;
+ let x2 = 0;
+ let y2 = 449;
+ let vY = 0.1;
+ let vX = 1;
+ let t1 = 0;
+ let t2 = 0;
+function mover(){
+    if(y<450){ 
+        x = x + dX;
+        y = y + dY;
+        angle = angle + dAngle;
+        movimiento.style.transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
+    }else{
+        x = x - dX;
+        y = 0;
+        angle = angle + dAngle;
+        movimiento.style.transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
+    }
+    if(y2   > 450){
+        t2 = 0;
+        y2 = 449;
+    }
+    else{ 
+    t1+= 0.1;
+    t2+= 0.1;
+    x2 = x2 + vX*t1;
+    y2 = y2 - (vY*-9.8*Math.pow(t2,2))/2;
+    angle = angle + dAngle;
+    obj2.style.transform = `translate(${x2}px, ${y2}px) rotate(${angle}deg)`;
+    }
+}
+
+let idMovimiento;
+let idColor;
+let corriendo = false;
+function start(){
+     corriendo = true;
+    idMovimiento = window.setInterval(mover,100);
+    //setInterval> ejecuta una función cada cierto tiempo. El tiempo se debe escribir e milisegundos
+    idColor = window.setInterval(cambiarColor,30);   
+}
+function stop(){
+    window.clearInterval(idMovimiento);
+    window.clearInterval(idColor);
+    corriendo = false;
+}
+window.onkeydown = function (evento){
+    if(evento.key == "w"){
+        if(corriendo)stop();
+        else start();
+    }
+};
